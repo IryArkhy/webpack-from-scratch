b6f9f727460de5af6ba31802ff857792ddb8139f
IryArkhy <37odri@gmail.com>
2020-06-12 12:16:55 +0300

    merge configs + css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpackMerge = require("webpack-merge");
+const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);// function returns 'require' which returns a function that you can call
 
-module.exports = ({ mode }) => ({
-    mode,
-    output: {
-        filename: "bundle.js"
-    },
-    plugins: [
-        new HtmlWebpackPlugin(),
-        new webpack.ProgressPlugin()
-    ]
-})
+module.exports = ({ mode, presets } = { mode: "production", presets: [] }) => {
+    return webpackMerge(
+        {
+            mode,
+            output: {
+                filename: "bundle.js"
+            },
+            plugins: [new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
+        },
+        modeConfig(mode)
+    );
+};
