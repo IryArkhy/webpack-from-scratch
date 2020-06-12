97397422cf64fccce99a660bf4ad9b7c670fa965
IryArkhy <37odri@gmail.com>
2020-06-12 14:26:02 +0300

    adding images url loader

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import nav from "./nav";
 import { footer } from "./footer";
 import makeButton from "./button";
 import { red, makeColor } from "./button-styles";
-import './css/footer.css'
+import makeImage from "./image";
+import imageURL from './cat.jpeg';
+import './css/footer.css';
 
 const button = makeButton('Yay a button!!!!');
 button.style = makeColor("cyan");
 document.body.appendChild(button);
 document.body.appendChild(footer);
+
+const image = makeImage(imageURL);
+document.body.appendChild(image);
